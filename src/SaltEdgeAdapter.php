413fbc0385eecf62d57c6db2a7a49de3ef8c88ab
Dmitry Monin <dmitry@17grad.com>
2019-02-11 12:05:27 +0100

    Initial commit

---
+++ src/SaltEdgeAdapter.php
@@ -0,0 +1,50 @@
+<?php namespace SaltEdge;
+
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Client;
+
+class SaltEdgeAdapter {
+  private $appId;
+  private $appSecret;
+
+  public function __construct($config) {
+    $this->appId = $config['app_id'];
+    $this->appSecret = $config['secret'];
+
+  }
+
+  public function api($method, $path, $data = []) {
+    $client = new Client();
+    $headers = [
+        'Accept' => 'application/json',
+        'Content-type' => 'application/json',
+        'App-id' => $this->appId,
+        'Secret' => $this->appSecret
+      ];
+
+    try {
+      $res = $client->request($method, 'https://www.saltedge.com/api/v4/' . $path, [
+        'headers' => [
+          'Accept' => 'application/json',
+          'Content-type' => 'application/json',
+          'App-id' => $this->appId,
+          'Secret' => $this->appSecret
+        ],
+        'query' => $method == 'get' ? $data : [],
+        'json' => $method == 'post' ? $data : []
+      ]);
+
+      return json_decode($res->getBody()->getContents(), true);
+    } catch (RequestException $exc) {
+
+      if ($exc->hasResponse()) {
+        return json_decode($exc->getResponse()->getBody()->getContents(), true);
+      }
+      else {
+        throw $exc;
+      }
+    }
+
+
+  }
+}
